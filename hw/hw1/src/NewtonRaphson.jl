ee5649fce92b459fccb2f4517d9865205a71511c
Arthur Lui <luiarthur@gmail.com>
2016-10-05 21:08:43 -0700

    yes

--- hw/hw1/src/NewtonRaphson.jl
+++ hw/hw1/src/NewtonRaphson.jl
@@ -1,19 +1,49 @@
 module NewtonRaphson
 
-  using Lazy
-  export optim
+using Lazy
+export optim
 
-  function optim(init,tprime,tprime2,eps=1E-3)
-    @bounce function loop(t0)
-      t1 = t0 - tprime(t0) / tprime2(t)
-      if abs(t1-t0) < eps 
-        t1
-      else
-        loop(t1)
-      end
+"""
+    optim(init, update; eps=1E-3, maxIts=1000, printIts=false)
+
+Newton Raphson Optimizer finds maxima by iteratively computing
+
+``θ_{new} = θ_{old} - f(θ_{old})``
+ 
+where ``f(x) = t'(x)/ t''(x)``.
+
+The algorithm stops when either
+
+* convergence has occured, OR
+* the number of iterations takes too long (> maxIts).
+
+# Arguments
+* `init::Real`    : initial value for newton raphson optimizer
+* `update`        : is the function ``t'(θ) / t''(θ)``
+* `eps::Real`     : how much the current iteration has to be to the 
+                    previous before stopping. (default: 1000)
+* `maxIts::Int`   : maximum number of iterations before stopping. (default: 1000)
+* `printIts::Bool`: whether or not info should be printed. (default: false)
+
+"""
+function optim(init, update; eps=1E-3, maxIts = 1000, printIts = false)
+
+  @bounce function loop(t0,it)
+    t1 = t0 - update(t0)
+
+    if it == maxIts
+      warn("Iteration: ",it," - Not Converged.")
+      t1
+    elseif abs(t1-t0) < eps
+      println("Converged in ",it," iterations.")
+      t1
+    else
+      loop(t1,it+1)
     end
 
-    loop(init)
   end
+
+  loop(init,1)
+end
 
 end
--- hw/hw1/src/hw1.jl
+++ hw/hw1/src/hw1.jl
@@ -1,10 +1,91 @@
-module hw1
-
 include("NewtonRaphson.jl")
 
 using Distributions, NewtonRaphson
 
-n = 100
-y = rand(Cauchy(5),100)
+y = [ -.774, .597, 7.575, .397, -.865, -.318, -.125, .961, 1.039 ]
+n = length(y)
 
+function t1(x,t)
+  num = 2*(x-t)
+  denom = 1 + (x-t).^2
+  sum( num ./ denom )
 end
+
+function t2(x,t)
+  num = 2 * (x-t-1) .* (x-t+1)
+  denom = (1 + (x-t).^2).^2
+  - sum( num ./ denom )
+end
+
+# likelihood function:
+function loglike_gen(x,t)
+  n = length(x)
+  -n*log(pi) - sum(log( 1 + (x-t).^2 ))
+end
+loglike(t) = loglike_gen(y,t)
+
+# update function for for newton raphson
+update(t::Real) = t1(y,t) / t2(y,t)
+
+# updates for scoring method
+J(x,t) = -length(x)/8
+score(t::Real) = t1(y,t) / J(y,t)
+
+#=
+include("hw1.jl")
+
+using RCall
+
+B = 100000
+@rput loglike B
+
+
+R" xx <- seq(-5,8,length=B) "
+R" ll <- sapply(xx,loglike) "
+R"""plot(xx,ll,type='l',fg='grey',bty='n',cex.lab=1.3,
+         xlab=expression(theta),ylab='likelihood',lwd=2,
+         col='grey30')"""
+R" mx <- xx[which.max(ll)] "
+R" abline(v=mx,col='red',lwd=2) "
+R"""legend('topright',legend=paste('maximum at theta =', round(mx,3)), 
+           text.col='grey30',bty='n',cex=1.5)"""
+
+# Newton
+optim(0, update, eps=1E-3, maxIts = B, printIts=true)
+optim(.18, update, eps=1E-3, maxIts = B, printIts=true)
+optim(10, update, eps=1E-3, maxIts = B, printIts=true)
+
+# Fisher
+optim(0, score, eps=1E-3, maxIts = B, printIts=true)
+optim(.18, score, eps=1E-3, maxIts = B, printIts=true)
+optim(10, score, eps=1E-3, maxIts = B, printIts=true)
+
+#---------------
+y2 = [0.0, 5, 9]
+loglike2(t) = loglike_gen(y2,t)
+update2(t::Real) = t1(y2,t) / t2(y2,t)
+score2(t::Real) = t1(y2,t) / J(y2,t)
+
+@rput loglike2
+
+R" xx <- seq(-20,20,length=B) "
+R" ll <- sapply(xx,loglike2) "
+R"""plot(xx,ll,type='l',fg='grey',bty='n',cex.lab=1.3,
+         xlab=expression(theta),ylab='likelihood',lwd=2,
+         col='grey30')"""
+R" mx <- xx[which.max(ll)] "
+R" abline(v=mx,col='red',lwd=2) "
+R"""legend('topleft',legend=paste('maximum at theta =', round(mx,3)), 
+           text.col='grey30',bty='n',cex=1.5)"""
+
+# Newton 
+optim(-1,   update2, eps=1E-3, maxIts = B, printIts=true)
+optim(4.67, update2, eps=1E-3, maxIts = B, printIts=true)
+optim(10,   update2, eps=1E-3, maxIts = B, printIts=true)
+
+# Fisher
+optim(-1,   score2, eps=1E-3, maxIts = B, printIts=true)
+optim(4.67, score2, eps=1E-3, maxIts = B, printIts=true)
+optim(10,   score2, eps=1E-3, maxIts = B, printIts=true)
+
+=#
