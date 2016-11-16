dc3a02111fb8d8f9017cf00d35ad3f59dbce7bd9
Arthur Lui <luiarthur@gmail.com>
2016-11-15 18:52:09 -0800

    yes

--- hw/hw3/src/Q2.jl
+++ hw/hw3/src/Q2.jl
@@ -1,7 +1,12 @@
 using BayesLM, Distributions, RCall
 sym(M::Matrix{Float64}) = (M' + M) / 2
 R"library(rcommon)"
-plot = R"plotPosts"
+plotpost = R"plotPosts"
+plot= R"plot"
+points= R"points"
+add_errbar = R"add.errbar"
+color_btwn = R"color.btwn"
+rgb = R"rgb"
 
 srand(1);
 
@@ -27,7 +32,7 @@ end
 @time model1 = glm(y, X, Σᵦ*.3, loglike1, B=2000,burn=10000);
 summary(model1)
 β1 = hcat(map(m -> m.β, model1.post_params)...)'
-plot(β1,cnames=["Intercept","slope"])
+plotpost(β1,cnames=["Intercept","slope"]);
 
 # Model II: logit link
 function loglike2(y::Vector{Float64},Xb::Vector{Float64},θ::Hyper)
@@ -37,7 +42,7 @@ end
 @time model2 = glm(y, X, Σᵦ*.5, loglike2, B=2000,burn=10000);
 summary(model2)
 β2 = hcat(map(m -> m.β, model2.post_params)...)'
-plot(β2,cnames=["Intercept","slope"])
+plotpost(β2,cnames=["Intercept","slope"]);
 
 # Model III: modified logit link
 lp_θ(θ::Hyper) = log(θ[:α])*(2-1) - θ[:α]*.01 # Gamma log prior, a=2,b=.01
@@ -50,9 +55,52 @@ end
                    θ_bounds=[0. Inf], 
                    θ_names=[:α], 
                    θ_logprior=lp_θ,
-                   B=5000,burn=100000);
+                   B=2000,burn=10000,printFreq=10000);
 summary(model3)
 β3 = hcat(map(m -> m.β, model3.post_params)...)'
 α = map(m->m.θ[:α], model3.post_params)
-plot(β3,cnames=["Intercept","slope"])
-plot(α)
+plotpost(β3,cnames=["Intercept","slope"]);
+plotpost(α);
+
+# Plot ##################
+x0 = collect(linspace(1.65,1.9,1000))
+function predict_p(x0::Vector{Float64}, β::Matrix{Float64}, inv_link)
+  const x0b = [ones(x0) x0] * β'
+  return inv_link.(x0b)
+end
+function predict_p(x0::Vector{Float64}, β::Matrix{Float64}, α::Vector{Float64})
+  const x0b = [ones(x0) x0] * β'
+  const B = length(α)
+  return hcat([exp(α[b]*x0b[:,b]) ./ (1+exp(x0b[:,b])).^α[b] for b in 1:B]...)
+end
+
+# MATCH COLORS!!! FIXME
+p1 = predict_p(x0,β1,invcloglog)
+p1_ci = mapslices(p -> quantile(p,[.025,.975]), p1, 2)
+plot(x0,mean(p1,2),xlab="",ylab="",fg="grey",bty="n",col=2,typ="l",lwd=3)
+#plot(x0,mean(p1,2),xlab="",ylab="",fg="grey",bty="n",col=2,typ="n",lwd=3)
+color_btwn(x0,p1_ci[:,1],p1_ci[:,2],from=minimum(x0),to=maximum(x0),rgb(1,0,0,.4))
+
+p2 = predict_p(x0,β2,invlogit)
+p2_ci = mapslices(p -> quantile(p,[.025,.975]), p2, 2)
+points(x0,mean(p2,2),xlab="",ylab="",fg="grey",bty="n",col=3,typ="l",lwd=3)
+color_btwn(x0,p2_ci[:,1],p2_ci[:,2],from=minimum(x0),to=maximum(x0),rgb(0,1,0,.3))
+
+p3 = predict_p(x0,β3,α)
+p3_ci = mapslices(p -> quantile(p,[.025,.975]), p3, 2)
+points(x0,mean(p3,2),xlab="",ylab="",fg="grey",bty="n",col=4,typ="l",lwd=3)
+color_btwn(x0,p3_ci[:,1],p3_ci[:,2],from=minimum(x0),to=maximum(x0),rgb(0,0,1,.3))
+
+#R"""
+#points(beetles$logDose,beetles$prob,pch=20,col='grey30',cex=2)
+#legend("bottomright",legend=c("Data","","",""),cex=2,text.col=c("grey30",2:4), bty='n')
+#"""
+
+
+
+# Lethal Dose ##################################
+#function ld(q::Float64,x0::Vector{Float64},β::Matrix{Float64},F_inv)
+#  assert(length(x0) == size(β,2))
+#  const X0b = β*x0
+#  F_inv(q)
+#end
